5574663a11f45e6b2947e2c03d1350c0bec2f864
NycolasCruz <nycolascruzsilva@gmail.com>
2022-07-31 18:55:23 -0300

    feat: first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function App() {
+    return (
+        <div className="card-body m-4">
+            <div className="rounded-top header">
+                <div className="d-flex text-center flex-column text-white pt-8">
+                    <span className="fw-bold fs-3 pt-4">SISTEMA FINANCEIRO</span>
+                </div>
+            </div>
+
+            <div className="bg-white rounded body">
+                body
+            </div>
+        </div>
+    )
+}
+
