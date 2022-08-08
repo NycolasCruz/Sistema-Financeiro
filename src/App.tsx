afdf655de954244384595d83931577102c57567c
unknown <nycolascruzsilva@gmail.com>
2022-08-08 09:40:26 -0300

    style: ajuste na classe

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App() {
     return (
-        <div className="card-body newm-5">
+        <div className="card-body m-12">
             <div className="rounded-top bg-blue" style={{height: '8rem'}}>
                 <div className="d-flex text-center flex-column text-white pt-8">
                     <span className="fw-bold fs-3 pt-9">SISTEMA FINANCEIRO</span>
@@ -10,7 +10,7 @@ export default function App() {
             </div>
 
             <div className="bg-white rounded mb-9 mt-n1 py-6 px-9">
-                body
+                
             </div>
         </div>
     )
