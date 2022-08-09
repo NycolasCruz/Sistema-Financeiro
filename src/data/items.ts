53e237650bdb8b895e9d61d52eb462611a507389
NycolasCruz <nycolascruzsilva@gmail.com>
2022-08-09 16:26:23 -0300

    style: tradução das categorias

--- src/data/items.ts
+++ src/data/items.ts
@@ -3,31 +3,31 @@ import { Item } from '../types/Item';
 export const items: Item[] = [
     {
         date: new Date(2022, 8, 8),
-        category: 'food',
+        category: 'Comida',
         title: 'açaí',
         value: 6.5
     },
     {
         date: new Date(2022, 8, 9),
-        category: 'payment',
+        category: 'Pagamento',
         title: 'salário',
         value: 800
     },
     {
         date: new Date(2022, 8, 8),
-        category: 'food',
+        category: 'Comida',
         title: 'Pão de queijo',
         value: 3
     },
     {
         date: new Date(2022, 8, 12),
-        category: 'college',
+        category: 'Pagamento',
         title: 'Faculdade',
         value: 183.70
     },
     {
-        date: new Date(2022, 8, 17),
-        category: 'card',
+        date: new Date(2022, 9, 17),
+        category: 'Contas',
         title: 'Itaú',
         value: 253.76
     }
