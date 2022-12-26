6c70d6786fcc8f73720e41db2e7e58af067e7234
NycolasCruz <nycolascruzsilva@gmail.com>
2022-12-26 02:20:30 -0300

    feat: new enum

---
+++ src/enums/Categories.ts
@@ -0,0 +1,13 @@
+enum categoriesName {
+    Alimentação = 1,
+    Salário = 2,
+    Faculdade = 3,
+    CartãoDeCrédito = 4
+}
+
+export const Categories = {
+    [categoriesName.Alimentação]: 1,
+    [categoriesName.Salário]: 2,
+    [categoriesName.Faculdade]: 3,
+    [categoriesName.CartãoDeCrédito]: 4
+}
