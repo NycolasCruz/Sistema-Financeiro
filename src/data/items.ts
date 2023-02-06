6a732cc51559a7844a858dda0993beb28cabc621
NycolasCruz <nycolascruzsilva@gmail.com>
2023-02-05 21:00:13 -0300

    style: ajuste na tipagem

--- src/data/items.ts
+++ src/data/items.ts
@@ -1,40 +1,41 @@
+import dayjs from "dayjs";
 import { ItemProps } from "../types/ItemProps"
 
 export const items: ItemProps[] = [
     {
-        date: new Date(2023, 0, 8),
+        date: dayjs(new Date(2023, 1, 8),).format('YYYY-MM-DD'),
         category: 1,
         name: 'açaí',
         value: 7.5
     },
     {
-        date: new Date(2023, 0, 8),
+        date: dayjs(new Date(2023, 1, 8)).format('YYYY-MM-DD'),
         category: 2,
         name: 'salário',
         value: 1500.55
     },
     {
-        date: new Date(2023, 0, 14),
+        date: dayjs(new Date(2023, 1, 14)).format('YYYY-MM-DD'),
         category: 1,
         name: 'Pão de queijo',
         value: 3
     },
     {
-        date: new Date(2023, 0, 25),
+        date: dayjs(new Date(2023, 1, 25)).format('YYYY-MM-DD'),
         category: 3,
         name: 'Faculdade',
         value: 183.7
     },
     {
-        date: new Date(2023, 0, 17),
+        date: dayjs(new Date(2023, 1, 17)).format('YYYY-MM-DD'),
         category: 4,
         name: 'cartão Itaú',
         value: 253.75
     },
     {
-        date: new Date(2023, 0, 17),
+        date: dayjs(new Date(2023, 1, 17)).format('YYYY-MM-DD'),
         category: 5,
         name: 'Fundo imobiliário',
         value: 780.5
     }
-]
+];
