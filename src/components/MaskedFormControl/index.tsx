13688b203b6a199c849399f2dbb92acc33d5a293
Nycolas Cruz <92335205+NycolasCruz@users.noreply.github.com>
2023-04-10 11:25:50 -0300

    Update index.tsx

--- src/components/MaskedFormControl/index.tsx
+++ src/components/MaskedFormControl/index.tsx
@@ -1,5 +1,5 @@
 import { IMaskMixin } from "react-imask";
-import { Form } from "react-bootstrap";
+import Form from "react-bootstrap/Form";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const MaskedFormControl = IMaskMixin(({ inputRef, ...props }: any) => (
