10dbdc78f5e3ebdcf0ec4abc1b75e267ca0ba4fa
NycolasCruz <nycolascruzsilva@gmail.com>
2023-06-11 18:00:28 -0300

    style: helpers removed

--- src/helpers/index.tsx
+++ src/helpers/index.tsx
@@ -1,7 +1,4 @@
-import axios from "axios";
-
 import { ItemProps } from "@/types/ItemProps";
-import { CategoryProps } from "@/types/CategoryProps";
 
 export function FilterList(list: ItemProps[], currentDate: string) {
 	return list.filter((item) => {
@@ -14,16 +11,4 @@ export function FilterList(list: ItemProps[], currentDate: string) {
 
 export function uppercaseFirstLetter(word: string) {
 	return word[0].toUpperCase() + word.slice(1);
-}
-
-export async function getCategories() {
-	const { data } = await axios.get<CategoryProps[]>("http://localhost:3001/categories");
-
-	return data;
-}
-
-export async function getItems() {
-	const { data } = await axios.get<ItemProps[]>("http://localhost:3001/projects");
-
-	return data;
 }
